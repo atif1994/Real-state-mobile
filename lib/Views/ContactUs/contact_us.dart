e3ae5f84f6c98a6a74c9b3639f28e6332b5ce6fd
Amina030452 <120363262+Amina030452@users.noreply.github.com>
2023-01-05 16:15:05 +0500

    contact_us

--- lib/Views/ContactUs/contact_us.dart
+++ lib/Views/ContactUs/contact_us.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart';
-import 'package:prologic_29/Views/Drawer/googlemap.dart';
+import 'package:prologic_29/custom_widgets/custom_button.dart';
+import 'package:prologic_29/custom_widgets/custom_textfield.dart';
+import 'package:prologic_29/utils/styles/app_textstyles.dart';
+
+import '../../utils/constants/appcolors.dart';
+import 'Get_in_touch.dart';
 
 class contactus extends StatefulWidget {
   const contactus({super.key});
@@ -10,12 +16,18 @@ class contactus extends StatefulWidget {
 }
 
 class _contactusState extends State<contactus> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final subjectController = TextEditingController();
+  final MessageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.appthem,
         centerTitle: true,
-        title: const Text("Contact Us"),
+        title: Text("Contact Us", style: AppTextStyles.heading1),
       ),
       body: SafeArea(
           child: ListView(
@@ -26,86 +38,92 @@ class _contactusState extends State<contactus> {
               const SizedBox(
                 height: 40.0,
               ),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Name',
-                ),
+              CustomTextField(
+                editingController: nameController,
+                hintText: 'Name',
               ),
               const SizedBox(
                 height: 20.0,
               ),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Email',
-                ),
+              CustomTextField(
+                editingController: emailController,
+                hintText: 'Email',
               ),
               const SizedBox(
                 height: 20.0,
               ),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Phone Number',
-                ),
+              CustomTextField(
+                editingController: phoneNumberController,
+                hintText: 'Phone No',
               ),
               const SizedBox(
                 height: 20.0,
               ),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Subject',
-                ),
+              CustomTextField(
+                editingController: subjectController,
+                hintText: 'Subject',
               ),
               const SizedBox(
                 height: 20.0,
                 width: 300,
               ),
-              const TextField(
-                maxLines: 30,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Message',
-                ),
+              CustomTextField(
+                editingController: MessageController,
+                hintText: 'Message',
               ),
               const SizedBox(
                 height: 20,
               ),
               Column(children: <Widget>[
-                ButtonTheme(
-                    height: 50,
-                    disabledColor: const Color.fromARGB(255, 1, 10, 18),
-                    child: ElevatedButton(
-                      child: const Text("Send Message"),
-                      onPressed: () {
-                        makePostRequest();
-                      },
-                    )),
-                const SizedBox(
-                  height: 20,
+                CustomButton(
+                  text: 'send Message',
+                  onPressed: () {
+                    makePostRequest();
+                  },
                 ),
-                const Text('GET In Touch With Us'),
+                // ButtonTheme(
+                //     height: 50,
+                //     disabledColor: const Color.fromARGB(255, 1, 10, 18),
+                //     child: ElevatedButton(
+                //       child: const Text("Send Message"),
+                //       onPressed: () {
+                //         makePostRequest();
+                //       },
+                //     )),
+
                 const SizedBox(
                   height: 20,
                 ),
                 Column(children: <Widget>[
-                  ButtonTheme(
-                      height: 70,
-                      minWidth: 100,
-                      disabledColor: const Color.fromARGB(255, 1, 10, 18),
-                      child: ElevatedButton(
-                        child: const Text("GOOGLE MAP"),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const MapSample()));
-                          //  var url = Uri.parse(
-                          // "https://www.google.com/maps/d/viewer?mid=1_xJhYeS2A83UaCE9C2gVhCnWijI&hl=en_US&ll=53.30462100000002%2C-6.328125&z=17");
-                        },
-                      )),
+                  CustomButton(
+                    text: 'Google Map',
+                    onPressed: () {
+                      //  makePostRequest();
+                    },
+                  ),
+                  // ButtonTheme(
+                  //     height: 70,
+                  //     minWidth: 100,
+                  //     disabledColor: const Color.fromARGB(255, 1, 10, 18),
+                  //     child: ElevatedButton(
+                  //       child: const Text("GOOGLE MAP"),
+                  //       onPressed: () {
+                  //         Navigator.push(
+                  //             context,
+                  //             MaterialPageRoute(
+                  //                 builder: (context) => const MapSample()));
+                  //         //  var url = Uri.parse(
+                  //         // "https://www.google.com/maps/d/viewer?mid=1_xJhYeS2A83UaCE9C2gVhCnWijI&hl=en_US&ll=53.30462100000002%2C-6.328125&z=17");
+                  //       },
+                  //     )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                      onTap: () {
+                        Get.to(() => Getintouch);
+                      },
+                      child: const Text('Get In Touch With Us')),
                 ])
               ])
             ],
--- lib/custom_widgets/custom_button.dart
+++ lib/custom_widgets/custom_button.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:prologic_29/utils/constants/appcolors.dart';
+import 'package:prologic_29/utils/styles/app_textstyles.dart';
 
 class CustomButton extends StatelessWidget {
   final String? text;
@@ -16,7 +17,13 @@ class CustomButton extends StatelessWidget {
           backgroundColor: AppColors.appthem,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
-      child: Text(text ?? ""),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          text ?? "",
+          style: AppTextStyles.labelSmall.copyWith(color: AppColors.colorWhite),
+        ),
+      ),
       onPressed: () {
         onPressed;
       },
