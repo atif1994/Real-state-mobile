dffa74e8c532d305d23cbd37afa28b83c4d0b146
MaryamRahim322 <maryam.rahim@tecrux.net>
2023-01-06 12:56:00 +0500

    area guid screen added
    area guid screen added

--- lib/Views/area_guide/area_guid.dart
+++ lib/Views/area_guide/area_guid.dart
@@ -6,6 +6,7 @@ import 'package:prologic_29/utils/styles/app_textstyles.dart';
 import 'package:prologic_29/utils/styles/custom_decorations.dart';
 import 'package:sizer/sizer.dart';
 
+import '../../utils/constants/image_resources.dart';
 import '../AboutUs/about_us.dart';
 // import 'package:prologic_29/utils/styles/app_textstyles.dart';
 
@@ -32,34 +33,30 @@ class AreaGuide extends StatelessWidget {
                   Text(
                     'Find By Locations',
                     style: AppTextStyles.heading1.copyWith(
-                      fontSize: 20,
+                      fontSize: 22,
                       color: Colors.black,
                       fontWeight: FontWeight.w500,
                     ),
-                    // style: TextStyle(
-                    //   fontSize: 24,
-                    //   fontWeight: FontWeight.w500,
-                    // ),
                   ),
                   const SizedBox(
-                    height: 8,
+                    height: 4,
                   ),
                   Text(
                     'Find your dream home from your dream location',
-                    style: AppTextStyles.heading1
-                        .copyWith(fontSize: 15, color: Colors.black54),
+                    style: AppTextStyles.labelSmall
+                        .copyWith(fontSize: 15, color: Colors.black87),
                   ),
                 ],
               ),
             ),
           ),
-          const SizedBox(
-            height: 30,
+          SizedBox(
+            height: 3.0.h,
           ),
           ListView.builder(
             primary: false,
             shrinkWrap: true,
-            itemCount: 5,
+            itemCount: 3,
             itemBuilder: (context, index) {
               return Container(
                 margin: EdgeInsets.only(
@@ -74,17 +71,13 @@ class AreaGuide extends StatelessWidget {
                         SizedBox(
                           height: 200,
                           width: 100.w,
-
-                          // ignore: prefer_const_constructors
                           child: ClipRRect(
                             borderRadius: const BorderRadius.only(
                                 topLeft: Radius.circular(10),
                                 topRight: Radius.circular(10)),
-                            child: const Image(
-                              // ignore: prefer_const_constructors
-                              image: NetworkImage(
-                                  'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
-                              fit: BoxFit.fitWidth,
+                            child: Image.asset(
+                              AppImageResources.property,
+                              fit: BoxFit.cover,
                             ),
                           ),
                         ),
@@ -156,9 +149,244 @@ class AreaGuide extends StatelessWidget {
           const SizedBox(
             height: 40,
           ),
-          const CustomButton(
-            text: 'Browse More Locations',
-            onPressed: null,
+          // const CustomButton(
+          //   text: 'Browse More Locations',
+          //   onPressed: null,
+          // ),
+          // SizedBox(
+          //   height: 5.0.h,
+          // ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(left: 3.0.w, right: 3.0.w),
+              child: CustomButton(
+                onPressed: () {},
+                text: "Brows More Propertiese",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 2.0.h,
+          ),
+          Container(
+            // width: 96.0.w,
+            margin: EdgeInsets.only(left: 2.0.w, right: 2.0.w),
+            decoration: BoxDecoration(
+                color: AppColors.colorblue,
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 2.0.h,
+                ),
+                Container(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Explore Good Places',
+                          style: AppTextStyles.heading1.copyWith(
+                            fontSize: 22,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 1.0.h,
+                        ),
+                        Text(
+                          'Find your dream home from your dream location',
+                          style: AppTextStyles.heading1
+                              .copyWith(fontSize: 15, color: Colors.black54),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 3.0.h,
+                ),
+                ListView.builder(
+                  primary: false,
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(
+                          left: 3.0.w,
+                          right: 3.0.w,
+                          top: index == 0 ? 1.0.h : 2.0.h),
+                      height: 42.0.h,
+                      width: 70.0.w,
+                      decoration: CustomDecorations.mainCon,
+
+                      child: Stack(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 200,
+                                width: 100.w,
+
+                                // ignore: prefer_const_constructors
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(10),
+                                      topRight: Radius.circular(10)),
+                                  child: Image.asset(
+                                    AppImageResources.property,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 2.0.h,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(6.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      height: 3.5.h,
+                                      width: 22.0.w,
+                                      decoration: BoxDecoration(
+                                          color: AppColors.appthem,
+                                          borderRadius:
+                                              BorderRadius.circular(300)),
+                                      child: Center(
+                                          child: Text(
+                                        "For sale ",
+                                        style: AppTextStyles.labelSmall
+                                            .copyWith(
+                                                color: AppColors.colorWhite),
+                                      )),
+                                    ),
+                                    Text("PKR 160,00000",
+                                        style: AppTextStyles.heading1.copyWith(
+                                            color: AppColors.colorblack,
+                                            fontSize: 10.sp))
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                  left: 2.0.w,
+                                ),
+                                child: Text(
+                                  'New Property',
+                                  style: AppTextStyles.heading1.copyWith(
+                                    color: AppColors.colorblack,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 1.0.h,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 2.0.w),
+                                child: Row(
+                                  // crossAxisAlignment: CrossAxisAlignment.end,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Image.asset(
+                                      AppImageResources.bed,
+                                      height: 2.0.h,
+                                    ),
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Text(
+                                      "3 beds",
+                                      style: AppTextStyles.labelSmall
+                                          .copyWith(fontSize: 9.sp),
+                                    ),
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Image.asset(
+                                      AppImageResources.bath,
+                                      height: 2.0.h,
+                                    ),
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Text(
+                                      "Bath",
+                                      style: AppTextStyles.labelSmall
+                                          .copyWith(fontSize: 9.sp),
+                                    ),
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Image.asset(
+                                      AppImageResources.plots,
+                                      height: 2.0.h,
+                                    ),
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Text(
+                                      "Area",
+                                      style: AppTextStyles.labelSmall
+                                          .copyWith(fontSize: 9.sp),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              const Divider(),
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Row(
+                                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Image.asset(
+                                      AppImageResources.loc,
+                                      height: 2.0.h,
+                                    ),
+                                    SizedBox(
+                                      width: 2.0.w,
+                                    ),
+                                    Text(
+                                      "Islamabad,Punjab",
+                                      style: AppTextStyles.labelSmall,
+                                    ),
+                                    SizedBox(
+                                      width: 24.0.w,
+                                    ),
+                                    Container(
+                                      height: 3.5.h,
+                                      width: 22.0.w,
+                                      decoration: BoxDecoration(
+                                          color: AppColors.appthem,
+                                          borderRadius:
+                                              BorderRadius.circular(300)),
+                                      child: Center(
+                                          child: Text(
+                                        "View ",
+                                        style: AppTextStyles.labelSmall
+                                            .copyWith(
+                                                color: AppColors.colorWhite),
+                                      )),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      //
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       )),
