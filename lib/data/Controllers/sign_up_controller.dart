81650c462568a6a3c4985a228c23e3bd10a535fc
Hasnain-Mirrani07 <122273493+Hasnain-Mirrani-Tecrux@users.noreply.github.com>
2023-02-15 15:47:37 +0500

    .

--- lib/data/Controllers/sign_up_controller.dart
+++ lib/data/Controllers/sign_up_controller.dart
@@ -1,12 +1,17 @@
-import 'package:dio/dio.dart' as dio;
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import '../Models/user_model.dart';
-import '../Services/my_shared_preferences.dart';
-import 'auth_controller.dart';
+import 'package:prologic_29/Views/Auth/sign_in.dart';
+import 'package:prologic_29/data/Services/signup_services/signup_services.dart';
+import '../Models/signup_model/signup_model.dart';
 
 class SignUpController extends GetxController {
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
   final _formkey = GlobalKey<FormState>();
 
   final TextEditingController firstNameController = TextEditingController();
@@ -19,59 +24,83 @@ class SignUpController extends GetxController {
   final TextEditingController SateController = TextEditingController();
   final TextEditingController CityController = TextEditingController();
   RxBool isLoading = false.obs;
+  var signupModel = SignupModel();
+  RxString errSignup = ''.obs;
 
   signUp() async {
     print(CountryController.text);
     print(CityController.text);
     print(SateController.text);
     print(UserController.text);
-    isLoading(true);
+    isLoading.value = true;
+    var res = await SignupServices.signupServices(
+        emailController.text,
+        passwordController.text,
+        UserController.text,
+        firstNameController.text,
+        lastNameController.text,
+        CountryController.text,
+        SateController.text,
+        CityController.text,
+        phoneNumberController.text);
+    if (res is SignupModel) {
+      isLoading(false);
+      Fluttertoast.showToast(msg: "");
+      Get.snackbar('Succfully Create Account', "");
+      Get.to(SignIn());
+      return res;
+    } else {
+      isLoading(false);
+      errSignup.value = res;
+      return res;
+    }
+
 //https://test.ditllcae.com/backend/public/api/
-    dio.Response response = await dio.Dio().post(
-      'https://realestate.tecrux.net/api/v1/register',
-      data: {
-        'email': emailController.text.trim(),
-        'password': passwordController.text.trim(),
-        'username': UserController.text.trim(),
-        'first_name': firstNameController.text.trim(),
-        'last_name': lastNameController.text.trim(),
-        'country': CountryController.text.trim(),
-        'state': SateController.text.trim(),
-        'city': CityController.text.trim(),
-        'phone': phoneNumberController.text.trim(),
-        'role_id': 4,
-      },
-    );
+    // dio.Response response = await dio.Dio().post(
+    //   'https://realestate.tecrux.net/api/v1/register',
+    //   data: {
+    //     'email': emailController.text.trim(),
+    //     'password': passwordController.text.trim(),
+    //     'username': UserController.text.trim(),
+    //     'first_name': firstNameController.text.trim(),
+    //     'last_name': lastNameController.text.trim(),
+    //     'country': CountryController.text.trim(),
+    //     'state': SateController.text.trim(),
+    //     'city': CityController.text.trim(),
+    //     'phone': phoneNumberController.text.trim(),
+    //     'role_id': 4,
+    //   },
+    // );
 
-    final data = response.data;
+    // final data = response.data;
 
-    if (data['error'] == false) {
-      Fluttertoast.showToast(msg: 'Authorised');
-      isLoading(false);
-      final user = data['data'];
-      MySharedPreferences.storeUserData(
-          userModel: UserModel(
-        userId: user['id'],
-        // phone: user['phone'],
-        userName: user['username'],
-        firstname: user['first_name'],
-        lastname: user['last_name'],
-        email: user['email'],
-        // city: user['city'],
-        // country: user['country'],
-        // state: user['state'],
-      ));
-      Get.find<AuthController>().isUserSignedIn();
+    // if (data['error'] == false) {
+    //   Fluttertoast.showToast(msg: 'Authorised');
+    //   isLoading(false);
+    //   final user = data['data'];
+    //   MySharedPreferences.storeUserData(
+    //       userModel: UserModel(
+    //     userId: user['id'],
+    //     // phone: user['phone'],
+    //     userName: user['username'],
+    //     firstname: user['first_name'],
+    //     lastname: user['last_name'],
+    //     email: user['email'],
+    //     // city: user['city'],
+    //     // country: user['country'],
+    //     // state: user['state'],
+    //   ));
+    //   Get.find<AuthController>().isUserSignedIn();
 
-      Get.snackbar('Signed In', 'User is signed in');
-    }
+    //   Get.snackbar('Signed In', 'User is signed in');
+    // }
 
-    // print(data['data']);
+    // // print(data['data']);
 
-    else {
-      Fluttertoast.showToast(msg: ':UnAuthorised');
-      isLoading(false);
-      print("error");
-    }
+    // else {
+    //   Fluttertoast.showToast(msg: ':UnAuthorised');
+    //   isLoading(false);
+    //   print("error");
+    // }
   }
 }
---
+++ lib/data/Services/signup_services/signup_services.dart
@@ -0,0 +1,47 @@
+import 'package:prologic_29/utils/constants/base_client.dart';
+
+import '../../../utils/constants/app_urls.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../../Models/signup_model/signup_model.dart';
+
+class SignupServices {
+  static Future<dynamic> signupServices(
+    email,
+    pass,
+    username,
+    fname,
+    lname,
+    country,
+    state,
+    city,
+    phone,
+  ) async {
+    Map data = {
+      "email": email,
+      "password": pass,
+      "username": username,
+      "first_name": fname,
+      "last_name": lname,
+      "country": country,
+      "state": state,
+      "city": city,
+      "phone": phone,
+      "role_id": 4
+    };
+
+    var url = "${AppUrls.baseUrl}${AppUrls.postLikee}";
+    var res = await BaseClientClass.post(
+        "https://realestate.tecrux.solutions/api/v1/register", data);
+    try {
+      if (res is http.Response) {
+        return signupModelFromJson(res.body);
+      } else {
+        return res;
+      }
+    } catch (e) {
+      return e;
+    }
+  }
+}
